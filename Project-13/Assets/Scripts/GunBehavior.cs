54f167296c82f3cf49001dae8e19e10ebf2e19ca
Quentin <q.lambkin@gmail.com>
2021-12-14 18:20:07 -0500

    Breathing motion in gun, ADS, ammo and reload

--- Project-13/Assets/Scripts/GunBehavior.cs
+++ Project-13/Assets/Scripts/GunBehavior.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Collections;
 
 public class GunBehavior : MonoBehaviour
 {
@@ -7,14 +8,66 @@ public class GunBehavior : MonoBehaviour
     public float impactForce = 30f;
     public float fireRate = 15f;
 
+    public int maxAmmo = 10;
+    private int currentAmmo;
+    public float reloadTime = 1f;
+    private bool isReloading = false;
+    private bool isScoped = false;
+
     public Camera fpsCam;
     public ParticleSystem muzzleFlash;
     public GameObject impactEffect;
 
     private float nextTimeToFire = 0f;      //Fire rate cap
 
+    public Animator reloadAnim;             //Reload animation
+    public Animator scopeAnim;              //Scope animation
+
+    public GameObject scopeOverlay;
+    public GameObject weaponCamera;
+    public Camera mainCamera;
+
+    public float scopedFOV = 15f;
+    private float normalFOV;
+
+    void Start()
+    {
+        currentAmmo = maxAmmo;
+        normalFOV = mainCamera.fieldOfView;               //Initialize the normal field of view
+    }
+
+    void OnEnable()     //Allows for swapping weapons mid reload
+    {
+        isReloading = false;
+        reloadAnim.SetBool("Reloading", false);
+    }
+
     void Update()
     {
+        if (Input.GetButtonDown("Fire2"))       //Aim down scope
+        {
+            isScoped = !isScoped;
+            scopeAnim.SetBool("Scoped", isScoped);
+
+            if (isScoped)
+            {
+                StartCoroutine(OnScoped());     //Scope in
+            } else {
+                OnUnscoped();                   //Scope out
+            }
+        }
+
+        if (isReloading)
+        {
+            return;                             //Ignores command to reload if already reloading
+        }
+
+        if(currentAmmo <= 0)
+        {
+            StartCoroutine(Reload());           //Begins reloading
+            return;
+        }
+
         //Shoot when fired if allowed
         if (Input.GetButton("Fire1") && Time.time >= nextTimeToFire)
         {
@@ -23,9 +76,54 @@ public class GunBehavior : MonoBehaviour
         }
     }
 
+    IEnumerator OnScoped()
+    {
+        yield return new WaitForSeconds(.15f);      //Added lag when transitioning into scope
+        weaponCamera.SetActive(false);
+        scopeOverlay.SetActive(true);
+
+        mainCamera.fieldOfView = scopedFOV;               //Zoom in effect when scoped
+    }
+
+    void OnUnscoped()
+    {
+        scopeOverlay.SetActive(false);
+        weaponCamera.SetActive(true);
+
+        mainCamera.fieldOfView = normalFOV;
+    }
+
+    IEnumerator Reload()
+    {
+        isReloading = true;
+        Debug.Log("Reloading...");
+
+        yield return new WaitForSeconds(.25f);              //Added lag before reload time
+        scopeAnim.SetBool("Scoped", false);                 //Back out of scope
+        OnUnscoped();                                       //Back out of scope overlay
+        reloadAnim.SetBool("Reloading", true);              //Begin reloading
+
+        yield return new WaitForSeconds(reloadTime - .25f); //Reduced lag during reload time
+        
+        reloadAnim.SetBool("Reloading", false);             //Finish reloading
+        scopeAnim.SetBool("Scoped", isScoped);              //Scopes in if player was scoped in before reloading
+        yield return new WaitForSeconds(.25f);              //Added lag post reload time
+
+        if (isScoped)
+        {
+            StartCoroutine(OnScoped());                     //Scope overlay applied if player was scoped in before reloading
+        }
+
+        currentAmmo = maxAmmo;
+        isReloading = false;
+
+    }
+
     void Shoot()
     {
         muzzleFlash.Play();     //Show flash when fired
+
+        currentAmmo--;
 
         RaycastHit hit;
 
