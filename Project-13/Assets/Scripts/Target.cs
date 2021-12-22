458bf7e17c8da5bb74438b1b9906909c606f677b
Quentin <q.lambkin@gmail.com>
2021-12-22 18:05:20 -0500

    Health Text

--- Project-13/Assets/Scripts/Target.cs
+++ Project-13/Assets/Scripts/Target.cs
@@ -1,9 +1,16 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Target : MonoBehaviour
 {
 
     public float health = 50f;
+    public Text healthDispley;
+
+    public void Update()
+    {
+        healthDispley.text = health.ToString();
+    }
 
     public void TakeDamage(float amount)
     {
