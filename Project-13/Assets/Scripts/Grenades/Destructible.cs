889c3d49ff1e220d8e5b7c7cc15587f4c066ba03
Quentin <q.lambkin@gmail.com>
2021-12-22 13:42:38 -0500

    Weapon Scrpits

---
+++ Project-13/Assets/Scripts/Grenades/Destructible.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destructible : MonoBehaviour
+{
+    public GameObject destroyedVersion;       //Shattered version of the object
+
+    public void Destroy()
+    {
+        Instantiate(destroyedVersion, transform.position, transform.rotation);
+
+        Destroy(gameObject);
+    }
+
+}
