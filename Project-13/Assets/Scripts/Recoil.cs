f263f3b647e9910b770c26a403e49f2af9a0f0f1
Quentin <q.lambkin@gmail.com>
2021-12-21 19:51:56 -0500

    Reload

--- Project-13/Assets/Scripts/Recoil.cs
+++ Project-13/Assets/Scripts/Recoil.cs
@@ -2,20 +2,20 @@ using UnityEngine;
 
 public class Recoil : MonoBehaviour
 {
-    public bool isADS;                      //Aiming down sight boolean
+    public bool isADS;                  //Aiming down sight boolean
 
-    private Vector3 currentRotation;        //Roatations
+    private Vector3 currentRotation;    //Roatations
     private Vector3 targetRotation;
 
-    public float recoilX = -2;                   //Hip fire recoil
+    public float recoilX = -2;          //Hip fire recoil
     public float recoilY = 2;
     public float recoilZ = 0.35f;
 
-    public float adsRecoilX = -1.5f;                //ADS recoil
+    public float adsRecoilX = -1.5f;    //ADS recoil
     public float adsRecoilY = 1;
     public float adsRecoilZ = 0.3f;
 
-    public float snapiness = 6;                 //Settings
+    public float snapiness = 6;         //Settings
     public float returnSpeed = 2;
 
     void Update()
@@ -25,11 +25,11 @@ public class Recoil : MonoBehaviour
         transform.localRotation = Quaternion.Euler(currentRotation);                                        //begin movement from current target to new target
     }
 
-    public void RecoilFire()
+    public void RecoilFire()        //Randomized location of recoil pattern (less recoil when ADS)
     {
         if (isADS)
         {
-            targetRotation += new Vector3(adsRecoilX, Random.Range(-adsRecoilY, adsRecoilY), Random.Range(-adsRecoilZ, adsRecoilZ));
+            targetRotation += new Vector3(adsRecoilX, Random.Range(-adsRecoilY, adsRecoilY), Random.Range(-adsRecoilZ, adsRecoilZ));        
         } else
         {
             targetRotation += new Vector3(recoilX, Random.Range(-recoilY, recoilY), Random.Range(-recoilZ, recoilZ));
