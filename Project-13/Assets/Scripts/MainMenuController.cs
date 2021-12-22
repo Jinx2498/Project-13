c98554aaf7f1eb1db779904632debfc4e8aebd32
hassanuahmad <ahmad12p@uwindsor.ca>
2021-12-22 14:35:43 -0500

    completed main menu w/ music controls etc

---
+++ Project-13/Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+     public void HelpScene() 
+    {
+        SceneManager.LoadScene("Help");
+    }
+
+     public void OptionsScene() 
+    {
+        SceneManager.LoadScene("Options");
+    }
+
+     public void BackToMainMenu() 
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+        UnityEditor.EditorApplication.isPlaying = false;
+    }
+}
